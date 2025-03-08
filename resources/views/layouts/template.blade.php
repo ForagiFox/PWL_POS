ad5a35dc36694dbcedc6ad2b94511904d9755969
KnoyanMitsu <yanappuowo@yandex.com>
2025-03-08 15:58:25 +0000

    Praktikum-1

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{config('app.name', 'PWL Laravel Starter Code')}}</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
+</head>
+
+<body class="hold-transition sidebar-mini">
+
+    <div>
+    @include('layouts.header')
+
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{ url('/') }}" class="brand-link">
+      <img src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">PWL - Starter</span>
+    </a>
+
+@include('layouts.sidebar')
+</aside>
+
+<div class"content-wrapper">
+    @include('layouts.breadcrumb')
+    <section>
+        @yield('content')
+
+    </section>
+</div>
+@include('layouts.footer')
+
+    </div>
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+</body>
+</html>
